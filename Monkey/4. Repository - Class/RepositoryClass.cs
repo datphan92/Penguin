0c049ae3f8f0ea3648e5d24767e7d5a548384c19
Top <topnguyen92@gmail.com>
2017-08-15 09:59:06 +0800

    Update Repository Template

--- Monkey/4. Repository - Class/RepositoryClass.cs
+++ Monkey/4. Repository - Class/RepositoryClass.cs
@@ -20,12 +20,11 @@
 using $solutionname$.Data.Entities;
 using $solutionname$.Data.Interfaces;
 using Puppy.DependencyInjection.Attributes
-using Puppy.EF;
 
 namespace $rootnamespace$
 {
     [PerRequestDependency(ServiceType = typeof(I$safeitemname$))]
-    public class $safeitemname$: BaseEntityRepository<$fileinputname$Entity>, I$safeitemname$
+    public class $safeitemname$: EntityRepository<$fileinputname$Entity>, I$safeitemname$
 	{
         public $safeitemname$(IDbContext dbContext) : base(dbContext)
         {
