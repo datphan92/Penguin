d1e7bcb116209de370210550b32281a1d83bd28e
Top <topnguyen92@gmail.com>
2017-08-15 12:36:45 +0800

    Add Entity Map Class

---
+++ IDWF/4. Data - Entity Map/EntityMapClass.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using $solutionname$.Data.Entities;
+using Puppy.EF.Mapping;
+
+namespace $rootnamespace$
+{
+    public class $safeitemname$: IEntityTypeConfiguration<$fileinputname$Entity>
+	{
+        public void Map(EntityTypeBuilder<$fileinputname$Entity> builder)
+        {
+            builder.ToTable(nameof($fileinputname$Entity));
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Version).IsRowVersion();
+        }
+    }
+}
---
+++ Monkey/4. Data - Entity Map/EntityMapClass.cs
@@ -0,0 +1,36 @@
+﻿#region	License
+//------------------------------------------------------------------------------------------------
+// <License>
+//     <Copyright> $year$ © $company$ </Copyright>
+//     <Url> $companyurl$ </Url>
+//     <Author> $username$ </Author>
+//     <Project> $solutionname$ → Repository </Project>
+//     <File>
+//         <Name> $safeitemname$.cs </Name>
+//         <Created> $time$ </Created>
+//         <Key> $guid10$ </Key>
+//     </File>
+//     <Summary>
+//         $safeitemname$.cs
+//     </Summary>
+// <License>
+//------------------------------------------------------------------------------------------------
+#endregion License
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using $solutionname$.Data.Entities;
+using Puppy.EF.Mapping;
+
+namespace $rootnamespace$
+{
+    public class $safeitemname$: IEntityTypeConfiguration<$fileinputname$Entity>
+	{
+        public void Map(EntityTypeBuilder<$fileinputname$Entity> builder)
+        {
+            builder.ToTable(nameof($fileinputname$Entity));
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Version).IsRowVersion();
+        }
+    }
+}
